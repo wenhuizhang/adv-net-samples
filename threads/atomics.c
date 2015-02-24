3885db3f40cbfcd223aaa3572b9a75198c3ea6a5
wenhuizhang <wenhui@gwmail.gwu.edu>
2015-02-24 15:50:04 -0500

    Atomics updated with timer location

--- threads/atomics.c
+++ threads/atomics.c
@@ -13,11 +13,10 @@
 #include <time.h>
 
 
-#define num_threads 10
-
 
 int ctr;
 pthread_mutex_t count_mutex;
+clock_t start_t, end_t, acc_lock_t, acc_atomic_t;
 
 
 
@@ -25,24 +24,26 @@ pthread_mutex_t count_mutex;
 /* thread_lock: thread function for multi-thread generation */
 void thread_lock()
 {
-    
+    start_t = clock();
     pthread_mutex_lock(&count_mutex);
     
     ctr = ctr + 1;
-    printf("%d \n", ctr);
+    //printf("%d \n", ctr);
     
     pthread_mutex_unlock(&count_mutex);
-    
+    end_t = clock();
+    acc_lock_t = acc_lock_t + (end_t - start_t);
 }
 
 
 /* thread_atomic: thread function for multi-thread generation */
 void thread_atomic()
 {
-    
+    start_t = clock();
     __sync_add_and_fetch(&ctr, 1);
-    printf("%d \n", ctr);
-    
+    //printf("%d \n", ctr);
+    end_t = clock();
+    acc_atomic_t = acc_atomic_t + (end_t - start_t);
     
 }
 
@@ -69,25 +70,26 @@ void thread_atomic()
 
 
 
-int main(void)
+int atomic_comp(int num_threads)
 {
-    clock_t start_t, end_t;
+    acc_lock_t = 0;
+    acc_atomic_t = 0;
+    
+    printf("Start of %d thread calculating...\n", num_threads);
     double total_t_lock, total_t_unlock; //time counter
     int rc;
     int tnum; //counter for threads
     pthread_mutex_init(&count_mutex, NULL); //initialize mutex
     
     
-    
     pthread_t  thread_info[num_threads];	// thread identifier
     
     /* Test for thread_incr_lock */
-   
-    printf("Starting of the program, start_t = %ld\n", start_t);
-     start_t = clock();
+    
+    start_t = clock();
     
     for (tnum = 0; tnum < num_threads; tnum++){
-        printf("create thread %d\n", tnum);
+        //printf("create thread %d\n", tnum);
         
         rc = pthread_create(&thread_info[tnum], NULL, (void*)&thread_lock, NULL);
         if (rc){
@@ -102,9 +104,8 @@ int main(void)
     }
     
     
-    end_t = clock();
-    printf("End of the process end_t = %ld\n", end_t);
-    total_t_lock = (double)((end_t - start_t) / (double)CLOCKS_PER_SEC);
+    
+    total_t_lock = (double)(acc_lock_t / (double)CLOCKS_PER_SEC);
     printf("Total time cost by running %d threads for increase function with lock implementation: %f\n sec", num_threads, total_t_lock);
     
     
@@ -113,11 +114,10 @@ int main(void)
     
     ctr = 0;
     
-    printf("Starting of the program, start_t = %ld\n", start_t);
     
     start_t = clock();
     for (tnum = 0; tnum < num_threads; tnum++){
-        printf("create thread %d\n", tnum);
+        //printf("create thread %d\n", tnum);
         
         rc = pthread_create(&thread_info[tnum], NULL, (void*)&thread_atomic, NULL);
         if (rc){
@@ -132,17 +132,32 @@ int main(void)
     }
     
     
-    end_t = clock();
-    printf("End of the process end_t = %ld\n", end_t);
-    total_t_unlock = (double)((end_t - start_t) / (double)CLOCKS_PER_SEC);
+    total_t_unlock = (double)(acc_atomic_t / (double)CLOCKS_PER_SEC);
     printf("Total time cost by running %d threads for increase function with atomic: %f sec\n", num_threads, total_t_unlock);
     
     
-
     
     printf("Mutex Time Cost Against Atomic by running %d threads for increase function: %f sec\n", num_threads, total_t_lock - total_t_unlock);
     
-    printf("Exiting of the program...\n");
+    
+    printf("Per thread Mutex Time Cost Against Atomic by running %d threads for increase function: %f sec\n", num_threads, (total_t_lock - total_t_unlock)/(double)num_threads);
+    
+    printf("End of %d thread calculating...\n", num_threads);
     
     return 0;
+}
+
+
+
+int main(void)
+{
+    printf("------------------------\n");
+    atomic_comp(20);
+    printf("------------------------\n");
+    atomic_comp(1);
+    printf("------------------------\n");
+    atomic_comp(1000);
+    
+    return 0;
+    
 }
